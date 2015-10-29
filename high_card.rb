71c9b03f358eaf0bf71503c22a1f8a7495035780
Peter Zhang <pzhang87@gmail.com>
2015-10-29 18:11:44 -0400

    attempt object oriented solution

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,43 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+deck = []
+
+suits.each do |suit|
+  ranks.each_with_index do |rank, index|
+    deck.push([rank, suit, index])
+  end
+end
+
+is_running = true;
+
+def game()
+  puts "Enter a player name, or type 'play', or 'quit'."
+  input = gets.to_s
+
+  if input == "play"
+    play_turn()
+  elsif input == "quit"
+    is_running = false
+  else
+    players = players.push(input)
+    puts "#{input} has joined the game."
+  end
+
+end
+
+
+def play_turn()
+  deck.shuffle
+  players.each do |player|
+    puts "#{player} played #{deck[player][0]} of #{deck[player][1]}!"
+
+  end
+end
+
+loop do
+  is_running == true ? game() : break
+end
